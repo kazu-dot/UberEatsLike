f9130e51e02b273346189cd52f7ca496f9950e2b
kazu-dot <takahashikazuhiro90@icoud.com>
2021-03-12 15:53:12 +0900

    APIのデータをindexページに反映

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,9 +1,75 @@
 import React, { Fragment } from 'react';
+// --- useReducerを追加 ---
+import React, { Fragment, useReducer, useEffect } from 'react';
+import styled from 'styled-components';
+
+// apis
+import { fetchRestaurants } from '../apis/restaurants';
+
+// reducers
+import {
+  initialState,
+  restaurantsActionTyps,
+  restaurantsReducer,
+} from '../reducers/restaurants';
+
+// images
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
 
 export const Restaurants = () => {
+
+  const [state, dispatch] = useReducer(restaurantsReducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: restaurantsActionTyps.FETCHING });
+    fetchRestaurants()
+      .then((data) =>
+        dispatch({
+          type: restaurantsActionTyps.FETCH_SUCCESS,
+          payload: {
+            restaurants: data.restaurants
+          }
+        })
+      )
+  }, [])
+
+
   return (
     <Fragment>
-      レストラン一覧
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+
+      {
+        state.restaurantsList.map(restaurant =>
+          <div>
+            {restaurant.name}
+          </div>
+        )
+      }
+
     </Fragment>
   )
 }
